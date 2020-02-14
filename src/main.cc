5fa785f9c28a9fdcf160f4ef537abaab3091a3f5
Murray Cumming <murrayc@murrayc.com>
2020-02-14 05:38:37 +0100

    Add dependencies

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,38 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 #include <cstdlib>
+#include <iostream>
 
 int main() {
+  glfwInit();
+
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  auto window = glfwCreateWindow(800, 600, "example",
+    nullptr, nullptr);
+
+  uint32_t extensionCount = 0;
+  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount,
+    nullptr);
+
+  std::cout << "extensions count: " << extensionCount << std::endl;
+
+  glm::mat4 matrix{};
+  glm::vec4 vec{};
+  auto test = matrix * vec;
+  std::cout << "text.x: " << test.x << std::endl;
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
   return EXIT_SUCCESS;
 }
