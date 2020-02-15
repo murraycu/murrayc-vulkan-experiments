f250e0ca158d54c3395d3e3fe743650c1ebc6cfd
Murray Cumming <murrayc@murrayc.com>
2020-02-16 00:06:18 +0100

    Use glfw

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,8 @@
 #include <vulkan/vulkan.h>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 #include <stdexcept>
 #include <functional>
 #include <iostream>
@@ -7,22 +11,43 @@
 class HelloTriangleApplication {
 public:
   void run() {
+    initWindow();
     initVulkan();
     mainLoop();
     cleanup();
   }
 
 private:
+  void initWindow() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window_ = glfwCreateWindow(WIDTH, HEIGHT, "example",
+      nullptr, nullptr);
+  }
+
   void initVulkan() {
   }
 
   void mainLoop() {
+    while (!glfwWindowShouldClose(window_)) {
+      glfwPollEvents();
+    }
   }
 
   void cleanup() {
+    glfwDestroyWindow(window_);
+    window_ = {};
+
+    glfwTerminate();
   }
 
+  GLFWwindow* window_{};
 
+  static constexpr int WIDTH = 600;
+  static constexpr int HEIGHT = 800;
 };
 
 int main() {
