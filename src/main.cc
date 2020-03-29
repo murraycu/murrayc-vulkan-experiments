2553d94711c57de553913f30baa8490a9a581d07
Murray Cumming <murrayc@murrayc.com>
2020-03-29 11:27:24 +0200

    Use VK_FORMAT_R8G8B8A8_SRGB instead of VK_FORMAT_R8G8B8A8_UNORM
    More recent versions of the Vulkan Tutorial do this, though I'm not sure
    why.

--- src/main.cc
+++ src/main.cc
@@ -1115,16 +1115,16 @@ private:
 
     stbi_image_free(pixels);
 
-    createImage(texWidth, texHeight, VK_FORMAT_R8G8B8A8_UNORM,
+    createImage(texWidth, texHeight, VK_FORMAT_R8G8B8A8_SRGB,
                 VK_IMAGE_TILING_OPTIMAL,
                 VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
                 VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, textureImage_,
                 textureImageMemory_);
-    transitionImageLayout(textureImage_, VK_FORMAT_R8G8B8A8_UNORM,
+    transitionImageLayout(textureImage_, VK_FORMAT_R8G8B8A8_SRGB,
                           VK_IMAGE_LAYOUT_UNDEFINED,
                           VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
     copyBufferToImage(stagingBuffer, textureImage_, texWidth, texHeight);
-    transitionImageLayout(textureImage_, VK_FORMAT_R8G8B8A8_UNORM,
+    transitionImageLayout(textureImage_, VK_FORMAT_R8G8B8A8_SRGB,
                           VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
                           VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
 
@@ -1133,7 +1133,7 @@ private:
   }
 
   void createTextureImageView() {
-    textureImageView_ = createImageView(textureImage_, VK_FORMAT_R8G8B8A8_UNORM,
+    textureImageView_ = createImageView(textureImage_, VK_FORMAT_R8G8B8A8_SRGB,
                                         VK_IMAGE_ASPECT_COLOR_BIT);
   }
 
